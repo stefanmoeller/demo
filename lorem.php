846768f1cf8987a6153fdd134e82a4dba2d6d447
stm <git@stefanmoeller.com>
2012-06-13 16:54:10 +0200

    some more testing

---
+++ lorem.php
@@ -0,0 +1,18 @@
+<?php
+
+$test = 'test';
+
+printf('<p>%s</p>', $test);
+
+?>
+
+<div id="page">
+	<div class="logo"></div>
+	<ul id="navigation">
+		<li><a href="lorem.php?toc=1">1</a></li>
+		<li><a href="lorem.php?toc=2">2</a></li>
+		<li><a href="lorem.php?toc=3">3</a></li>
+		<li><a href="lorem.php?toc=4">4</a></li>
+		<li><a href="lorem.php?toc=5">5</a></li>
+	</ul>
+</div>
